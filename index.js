7cca55d4e40ca84167de8f629bd3eab8f29999c9
Acauan Ribeiro <acauan.ribeiro@gmail.com>
2022-08-31 10:08:19 -0400

    Ajeitando index

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const alunos = require('./api/alunos');
+const alunos = require("./api/alunos");
 
-app.use(express.json({extended: false}));
+app.use(express.json({ extended: false }));
 
 app.use("/api/alunos", alunos);
+
+/**
+ * Index Page
+ */
+app.get("/", (req, res) => {
+  res.send(
+    `<h1>API PAGE - of Prof. Acauan Ribeiro</h1> <br/>
+    <h2>Lista de APIs válidas</h2> <br/>
+    <ul>
+      <li>'/api/alunos' | return 'Lista de alunos de exemplo' </li>
+    </ul>
+    `
+  );
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
